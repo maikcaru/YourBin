1fdef5423421b7b3912f10f368dff88bfd2f6ec0
maikcaru@gmail.com <bXh3SfTzVIuJqZD>
2016-02-08 12:39:52 +0000

    Included setdayof week method in arrows, added in variables for the points

--- app/src/main/java/maikcaru/yourbin/ArrowView.java
+++ app/src/main/java/maikcaru/yourbin/ArrowView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -16,16 +17,23 @@ public class ArrowView extends View {
 
     private Paint paint;
     private boolean flipped;
+    float leftPoint;
+    float rightPoint;
+    float step;
+    float middlePoint;
+
 
     public ArrowView(Context context) {
         super(context);
         createPaint();
+        setDayOfWeek(6);
+
     }
 
     public ArrowView(Context context, AttributeSet attrs) {
         super(context, attrs);
         createPaint();
-
+        setDayOfWeek(6);
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ArrowView, 0, 0);
         flipped = a.getBoolean(R.styleable.ArrowView_flipped, false);
 
@@ -36,42 +44,47 @@ public class ArrowView extends View {
         paint.setStrokeWidth(10);
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.BLACK);
-        paint.setTextSize(60);
+        paint.setTextSize(45);
         paint.setTextAlign(Paint.Align.CENTER);
     }
 
     protected void onDraw(Canvas canvas) {
 
-
-        int step = canvas.getWidth() / 14;
-        int height;
-        int otherHeight;
-
+        step = canvas.getWidth() / 14;
+        int topPoint;
+        int bottomPoint;
         int margin = 80;
 
         if (flipped) {
-            height = margin;
-            otherHeight = canvas.getHeight();
-            canvas.drawText("Today", step, height/1.5f, paint);
+            topPoint = margin;
+            bottomPoint = canvas.getHeight();
+            canvas.drawText("Today", middlePoint * step, topPoint/1.5f, paint);
         } else {
-            height = canvas.getHeight()-margin;
-            otherHeight = 0;
-            canvas.drawText("Collection", step,canvas.getHeight(), paint);
+            topPoint = canvas.getHeight() - margin;
+            bottomPoint = 0;
+            canvas.drawText("Collection", middlePoint  * step, canvas.getHeight(), paint);
         }
 
         Path path = new Path();
 
-        path.moveTo(step, otherHeight);
-        path.lineTo(1.5f * step, height);
-        path.lineTo(0.5f * step, height);
-        path.lineTo(step, otherHeight);
+        path.moveTo(middlePoint * step, bottomPoint);
+        path.lineTo(rightPoint * step, topPoint);
+        path.lineTo(leftPoint * step, topPoint);
+        path.lineTo(middlePoint * step, bottomPoint);
 
         canvas.drawPath(path,paint);
 
-
-
-
     }
+
+    public void setDayOfWeek(int dayOfWeek){
+
+        if (dayOfWeek >=0 || dayOfWeek < 7) {
+            leftPoint = (dayOfWeek * 2) + 0.5f;
+            rightPoint = (dayOfWeek * 2) + 1.5f;
+            middlePoint = (dayOfWeek * 2) + 1;
+        }
+    }
+
 
 
 }
