86099bd2c312b0124ead1723aa132500d9ebd1cb
michael.carr <bXh3SfTzVIuJqZD>
2016-02-11 17:36:13 +0000

    Added settings activity

--- app/src/main/java/maikcaru/yourbin/SettingsActivity.java
+++ app/src/main/java/maikcaru/yourbin/SettingsActivity.java
@@ -1,27 +1,15 @@
 package maikcaru.yourbin;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 
 public class SettingsActivity extends NavigationDrawerParent {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        setContentView(R.layout.activity_settings);
         createToolbar();
 
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-        }
-    }
 }
---
+++ app/src/main/java/maikcaru/yourbin/SettingsFragment.java
@@ -0,0 +1,17 @@
+package maikcaru.yourbin;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by michael.carr on 11/02/16.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
